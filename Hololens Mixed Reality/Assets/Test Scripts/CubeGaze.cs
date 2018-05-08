00d871aca67acf73524aa2ff23b677b0d1cb1189
TDL-LAPTOP <tdluce@mail.umhb.edu>
2018-05-07 20:10:39 -0500

    First Commit

---
+++ Hololens Mixed Reality/Assets/Test Scripts/CubeGaze.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGaze : MonoBehaviour {
+    public Color32 GazeColor;
+    public bool IsGaze;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnGaze()
+    {
+        if (IsGaze == false)
+        {
+            GetComponent<Renderer>().material.color = GazeColor;
+            IsGaze = true;
+        }
+    }
+
+    public void OffGaze()
+    {
+        if (IsGaze == true)
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            IsGaze = false;
+        }
+    }
+}
---
+++ Hololens Mixed Reality/Assets/VR Scripts/VRManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRManager : MonoBehaviour {
+    public GameObject OBJ;
+	// Use this for initialization
+	void Start () {
+        XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(
+                Camera.main.transform.position,
+                Camera.main.transform.forward,
+                out hitInfo,
+                20.0f,
+                Physics.DefaultRaycastLayers))
+        {
+            // If the Raycast has succeeded and hit a hologram
+            // hitInfo's point represents the position being gazed at
+            // hitInfo's collider GameObject represents the hologram being gazed at
+            //Debug.Log(hitInfo.collider);
+            hitInfo.collider.gameObject.SendMessage("OnGaze", gameObject, SendMessageOptions.DontRequireReceiver);
+            OBJ = hitInfo.collider.gameObject;
+        }
+
+        else
+        {
+            if (OBJ != null)
+            {
+                OBJ.SendMessage("OffGaze", gameObject, SendMessageOptions.DontRequireReceiver);
+                OBJ = null;
+            }
+        }
+    }
+}
