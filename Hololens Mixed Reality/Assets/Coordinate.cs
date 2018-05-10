4644c6e5f417575f7c25dbaae33871a4f5aee649
T <tdlmailacct@gmail.com>
2018-05-10 10:09:50 -0500

    Sixth commit

---
+++ Hololens Mixed Reality/Assets/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinate: MonoBehaviour {
+    public int x;
+    public int y;
+    public Material[,] Sphere;
+	// Use this for initialization
+
+    void LoadMaterial ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
